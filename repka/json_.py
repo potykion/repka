a24d69b092293db4a27a093c7de2cb172a7feef8
potykion <potykion@gmail.com>
2019-12-22 23:31:33 +0300

    #14, #15: typed JsonDictRepo

--- repka/json_.py
+++ repka/json_.py
@@ -1,25 +1,26 @@
 import json
 import os
-from typing import List, Dict, Callable
+from typing import Dict, Callable, Sequence, Union, TypeVar, Generic
+
+JsonSerializable = Union[Dict, Sequence, str, bool, int, float]
+T = TypeVar("T", bound=JsonSerializable)
 
 
-class DictJsonRepo:
+class DictJsonRepo(Generic[T]):
     def __init__(self, directory: str = None) -> None:
         self.directory = directory or os.getcwd()
 
-    def read(self, filename: str) -> List[Dict]:
+    def read(self, filename: str) -> T:
         with open(self._build_path(filename), encoding="utf-8") as f:
             return json.load(f)
 
-    def write(self, data: List[Dict], filename: str) -> List[Dict]:
+    def write(self, data: T, filename: str) -> T:
         with open(self._build_path(filename), "w", encoding="utf-8") as f:
             json.dump(data, f)
 
         return data
 
-    def read_or_write_default(
-        self, filename: str, default_factory: Callable[[], List[Dict]]
-    ) -> List[Dict]:
+    def read_or_write_default(self, filename: str, default_factory: Callable[[], T]) -> T:
         if os.path.exists(self._build_path(filename)):
             return self.read(filename)
         else:
--- tests/test_json.py
+++ tests/test_json.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from _pytest.pytester import Testdir
+from typing import Sequence, Dict
 
 from repka.json_ import DictJsonRepo
 
@@ -10,7 +11,7 @@ def test_dict_json_repo_reads_json_file_as_list_of_dicts(testdir: Testdir) -> No
     data = [{"field": "value"}]
     file = testdir.makefile(".json", json.dumps(data))
 
-    repo = DictJsonRepo()
+    repo: DictJsonRepo[Sequence[Dict]] = DictJsonRepo()
 
     assert repo.read(file) == data
 
@@ -19,7 +20,7 @@ def test_dict_json_repo_write_list_of_dicts_as_json(testdir: Testdir) -> None:
     data = [{"field": "value"}]
     file = "sam.json"
 
-    repo = DictJsonRepo(str(testdir.tmpdir))
+    repo: DictJsonRepo[Sequence[Dict]] = DictJsonRepo(str(testdir.tmpdir))
     repo.write(data, file)
 
     assert repo.read(file) == data
@@ -28,7 +29,7 @@ def test_dict_json_repo_write_list_of_dicts_as_json(testdir: Testdir) -> None:
 def test_dict_json_repo_creates_file_if_no_file(testdir: Testdir) -> None:
     file = "sam.json"
 
-    repo = DictJsonRepo(str(testdir.tmpdir))
+    repo: DictJsonRepo[Sequence[Dict]] = DictJsonRepo(str(testdir.tmpdir))
     repo.read_or_write_default(file, lambda: [{"field": "value"}])
 
     assert os.path.exists(file)
@@ -38,6 +39,6 @@ def test_dict_json_repo_reads_file_if_exist(testdir: Testdir) -> None:
     data = [{"field": "value"}]
     file = testdir.makefile(".json", json.dumps(data))
 
-    repo = DictJsonRepo()
+    repo: DictJsonRepo[Sequence[Dict]] = DictJsonRepo()
 
     assert repo.read_or_write_default(file, lambda: []) == data
