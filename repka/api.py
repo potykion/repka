90e6bbf9149a2454c461a7bc94a76159f33e71b7
potykion <potykion@gmail.com>
2020-02-24 12:16:08 +0300

    Add AiopgRepo import to api

--- repka/api.py
+++ repka/api.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from repka.repositories.aiopg_ import AiopgRepository as BaseRepository
+from repka.repositories.aiopg_ import AiopgRepository
 from repka.repositories.base import IdModel
