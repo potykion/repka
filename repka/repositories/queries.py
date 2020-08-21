899e5739305c92d211fc55ed56c71bbd51d8e35c
potykion <potykion@gmail.com>
2020-08-21 16:37:01 +0300

    Document queries.py

--- repka/repositories/queries.py
+++ repka/repositories/queries.py
@@ -17,12 +17,15 @@ SqlAlchemyQuery = Union[ClauseElement]
 
 @dataclass
 class SelectQuery:
+    """SQL SELECT query with customizable filters, orders, columns"""
+
     table: Table
     filters: Filters = field(default_factory=list)
     orders: Columns = field(default_factory=list)
     select_columns: Columns = field(default_factory=list)
 
     def __call__(self) -> SqlAlchemyQuery:
+        """Create SQL SELECT query"""
         select_columns = self.select_columns if self.select_columns else [self.table]
         query = sa.select(select_columns)
         query = self.apply_filters(query, self.filters)
@@ -30,21 +33,26 @@ class SelectQuery:
         return query
 
     @staticmethod
-    def apply_filters(query: ClauseElement, filters: Filters) -> ClauseElement:
+    def apply_filters(query: SqlAlchemyQuery, filters: Filters) -> ClauseElement:
+        """Append WHERE clause to query"""
         return reduce(lambda query_, filter_: query_.where(filter_), filters, query)
 
     @staticmethod
-    def apply_orders(query: ClauseElement, orders: Columns) -> ClauseElement:
+    def apply_orders(query: SqlAlchemyQuery, orders: Columns) -> ClauseElement:
+        """Append ORDER BY clause to query"""
         return reduce(lambda query_, order_by: query_.order_by(order_by), orders, query)
 
 
 @dataclass
 class InsertQuery:
+    """SQL INSERT query with customizable insert values and returning columns"""
+
     table: Table
     insert_values: Mapping
     returning_columns: Columns = field(default_factory=list)
 
     def __call__(self) -> SqlAlchemyQuery:
+        """Create SQL INSERT query"""
         query = self.table.insert().values(self.insert_values)
         if self.returning_columns:
             query = query.returning(*self.returning_columns)
@@ -53,11 +61,14 @@ class InsertQuery:
 
 @dataclass
 class InsertManyQuery:
+    """Same as InsertQuery, but insert multiple values"""
+
     table: Table
     insert_values: Sequence[Mapping]
     returning_columns: Columns = field(default_factory=list)
 
     def __call__(self) -> SqlAlchemyQuery:
+        """Create SQL INSERT query"""
         query = self.table.insert().values(self.insert_values)
         if self.returning_columns:
             query = query.returning(*self.returning_columns)
@@ -66,13 +77,17 @@ class InsertManyQuery:
 
 @dataclass
 class UpdateQuery:
+    """SQL UPDATE query with customizable update values and filters"""
+
     table: Table
     update_values: Mapping
     filters: Filters
 
     def __call__(self) -> SqlAlchemyQuery:
-        query = self.table.update().values(self.update_values).where(sa.and_(*self.filters))
-        return query
+        """Create SQL UPDATE query"""
+        return SelectQuery.apply_filters(
+            self.table.update().values(self.update_values), self.filters
+        )
 
     @classmethod
     def by_id(
@@ -85,10 +100,17 @@ class UpdateQuery:
 
 @dataclass
 class DeleteQuery:
+    """SQL DELETE query with customizable filters"""
+
     table: Table
     filters: Union[Filters, Sequence[None]]
 
     def __call__(self) -> SqlAlchemyQuery:
+        """
+        Create SQL DELETE query
+
+        :raise ValueError if {filters} is empty. To delete all table rows pass {filters} = [None]
+        """
         if not len(self.filters):
             raise ValueError(
                 """No filters set, are you sure you want to delete all table rows?
